3487e169c9a932f664356eb7040fd973a1056aef
Vivek Kumar Singh <vivsingh1@publicisgroupe.net>
2018-05-22 16:01:23 +0530

    Added jetty server

--- price-stream/src/main/java/com/sapient/retail/price/service/kafka/KafkaPriceService.java
+++ price-stream/src/main/java/com/sapient/retail/price/service/kafka/KafkaPriceService.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.sapient.retail.price.service.kafka.KafkaConfiguration;
@@ -14,6 +15,9 @@ import com.sapient.retail.price.common.model.Price;
 @Service
 public class KafkaPriceService {
 
+	@Value (value = "${kafka.intopic}")
+	private String priceKafkaInTopic;
+	
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	private final KafkaConfiguration kafkaConfig;
 
@@ -27,7 +31,7 @@ public class KafkaPriceService {
 		try {
 			JsonNode jsonObj = new ObjectMapper().valueToTree(priceData);
 			final ProducerRecord<String, JsonNode> priceRecord = new ProducerRecord<String, JsonNode>(
-					"price-data-stream-IN", priceData.getSkuId(), jsonObj);
+					priceKafkaInTopic , priceData.getSkuId(), jsonObj);
 			priceProducer.send(priceRecord, (metadata, exception) -> {
 				if (null != metadata) {
 					logger.info("Sent price data {}", priceData.getSkuId() + " :: " + priceData.getPrice());
