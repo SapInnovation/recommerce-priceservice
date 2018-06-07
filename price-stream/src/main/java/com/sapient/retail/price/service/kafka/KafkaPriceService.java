e2fb61f3c619bbabed6678aeb656640e37ea11ad
Vivek Kumar Singh <vivsingh1@publicisgroupe.net>
2018-06-07 17:16:46 +0530

    Added end point to create topic in kafka

--- price-stream/src/main/java/com/sapient/retail/price/service/kafka/KafkaPriceService.java
+++ price-stream/src/main/java/com/sapient/retail/price/service/kafka/KafkaPriceService.java
@@ -1,5 +1,9 @@
 package com.sapient.retail.price.service.kafka;
 
+import java.util.Properties;
+
+import org.I0Itec.zkclient.ZkClient;
+import kafka.utils.ZKStringSerializer$;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.Logger;
@@ -8,6 +12,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.sapient.retail.price.service.kafka.KafkaConfiguration;
+
+import kafka.admin.AdminUtils;
+
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sapient.retail.price.common.model.Price;
@@ -17,6 +24,12 @@ public class KafkaPriceService {
 
 	@Value (value = "${kafka.intopic}")
 	private String priceKafkaInTopic;
+	
+	@Value(value = "${kafka.server:127.0.0.0:9092}")
+	private String kafkaBrokerHosts;
+	
+	@Value(value = "${kafka.zookeeper:127.0.0.0:2181}")
+	private String zookeeperHosts;
 	
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	private final KafkaConfiguration kafkaConfig;
@@ -44,6 +57,21 @@ public class KafkaPriceService {
 			priceProducer.close();
 		}
 
+	}
+	
+	public void createTopicInKafka (String topicName) {
+        int sessionTimeOut = 10000;
+        int connectionTimeOut = 10000;
+        logger.info("zookeeperHosts:{}", zookeeperHosts);
+        ZkClient zkClient = new ZkClient(zookeeperHosts, sessionTimeOut, connectionTimeOut, ZKStringSerializer$.MODULE$);
+        if (!AdminUtils.topicExists(zkClient, topicName)) {
+            int replicationFactor = kafkaBrokerHosts.split(",").length;
+            AdminUtils.createTopic(zkClient, topicName, 1, replicationFactor, new Properties());
+        } else {
+            logger.info(topicName +" is available hence no changes are done");
+        }
+        logger.info("Topic Details:{}", AdminUtils.fetchTopicMetadataFromZk(topicName, zkClient));
+        zkClient.close();
 	}
 
 }
